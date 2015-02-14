bb203eda78d648f71423199d3b740acb55c3d532
Kelly O'Briant <kellyobriant@gmail.com>
2015-02-13 22:32:51 -0500

    Bad Seed Data Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,24 @@
 require 'faker'
 
-50.times do 
-	Post.create!(
-		title: Faker::Lorem.sentence,
-		body: Faker::Lorem.paragraph
-		)
-end
-posts = Post.all
+#50.times do 
+#	Post.create!(
+#		title: Faker::Lorem.sentence,
+#		body: Faker::Lorem.paragraph
+#		)
+#end
+#posts = Post.all
 
-100.times do 
-	Comment.create!(
-		post: posts.sample,
-		body: Faker::Lorem.paragraph
-		)
-end
+#100.times do 
+#	Comment.create!(
+#		post: posts.sample,
+#		body: Faker::Lorem.paragraph
+#		)
+#end
+
+Post.create!(
+	title: "You Won't Believe the Three Salad Dressings I got from Whole Foods Today",
+	body: "1. Ranch Chia  2. Apple Fennel  3. Balsamic Thyme"
+	)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
