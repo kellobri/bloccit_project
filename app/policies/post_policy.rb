803a03eadfc37ece637084476f59b9a7fdeb53ad
Kelly O'Briant <kellyobriant@gmail.com>
2015-02-17 16:40:47 -0500

    Complete Authorization Assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,4 +4,25 @@ class PostPolicy < ApplicationPolicy
 		true
 	end
 	
+	class Scope < Scope
+		#attr_reader :user, :scope
+
+		#def initialize(user, scope)
+		#	@user = user
+		#	@scope = scope
+		#end
+
+		def resolve 
+			if user.admin?
+				scope.all
+			else
+				scope.where(:user_id => true)
+			end
+		end
+	end
+
+#	def update?
+#		user.admin? or not post.published?
+#	end
+
 end
