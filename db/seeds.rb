fafffcd5a68fb3c88313341176dbedc961bf9b01
Kelly O'Briant <kellyobriant@gmail.com>
2015-02-16 22:38:18 -0500

    Added user association to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,49 @@
 require 'faker'
 
-#50.times do 
-#	Post.create!(
-#		title: Faker::Lorem.sentence,
-#		body: Faker::Lorem.paragraph
-#		)
-#end
-#posts = Post.all
+# Create Users
+
+5.times do
+	user = User.new(
+		name: Faker::Name.name,
+		email: Faker::Internet.email,
+		password: Faker::Lorem.characters(10)
+		)
+	user.skip_confirmation!
+	user.save!
+end
+users = User.all
+
+50.times do 
+	Post.create!(
+		user: users.sample,
+		title: Faker::Lorem.sentence,
+		body: Faker::Lorem.paragraph
+		)
+end
+posts = Post.all
 
 #100.times do 
 #	Comment.create!(
+#       user: users.sample,
 #		post: posts.sample,
 #		body: Faker::Lorem.paragraph
 #		)
 #end
 
-Post.create!(
-	title: "You Won't Believe the Three Salad Dressings I got from Whole Foods Today",
-	body: "1. Ranch Chia  2. Apple Fennel  3. Balsamic Thyme"
+#Post.create!(
+#	title: "You Won't Believe the Three Salad Dressings I got from Whole Foods Today",
+#	body: "1. Ranch Chia  2. Apple Fennel  3. Balsamic Thyme"
+#	)
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+	email: 'kellyobriant@gmail.com',
+	password: 'helloworld'
 	)
+	
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
