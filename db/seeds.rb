c25a7a8d30904403050e51df5db50892a4674a91
Kelly O'Briant <kellyobriant@gmail.com>
2015-02-17 21:01:16 -0500

    Nested posts into Topics and refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,34 +13,63 @@ require 'faker'
 end
 users = User.all
 
+15.times do
+	Topic.create!(
+		name: Faker::Lorem.sentence,
+		description: Faker::Lorem.paragraph
+	)
+end
+topics = Topic.all
+
 50.times do 
 	Post.create!(
 		user: users.sample,
+		topic: topics.sample,
 		title: Faker::Lorem.sentence,
 		body: Faker::Lorem.paragraph
 		)
 end
 posts = Post.all
 
-#100.times do 
-#	Comment.create!(
-#       user: users.sample,
-#		post: posts.sample,
-#		body: Faker::Lorem.paragraph
-#		)
-#end
+100.times do 
+	Comment.create!(
+        #user: users.sample,
+		post: posts.sample,
+		body: Faker::Lorem.paragraph
+		)
+end
 
 #Post.create!(
 #	title: "You Won't Believe the Three Salad Dressings I got from Whole Foods Today",
 #	body: "1. Ranch Chia  2. Apple Fennel  3. Balsamic Thyme"
 #	)
 
-user = User.first
-user.skip_reconfirmation!
-user.update_attributes!(
-	email: 'kellyobriant@gmail.com',
+admin = User.new(
+	name: 'Admin User',
+	email: 'admin@example.com',
+	password: 'helloworld',
+	role: 'admin'
+	)
+admin.skip_confirmation!
+admin.save!
+
+moderator = User.new(
+	name: 'Moderator User',
+	email: 'moderator@example.com',
+	password: 'helloworld',
+	role: 'moderator'
+	)
+moderator.skip_confirmation!
+moderator.save!
+
+member = User.new(
+	name: 'Member User',
+	email: 'member@example.com',
 	password: 'helloworld'
 	)
+
+member.skip_confirmation!
+member.save!
 	
 
 puts "Seed finished"
