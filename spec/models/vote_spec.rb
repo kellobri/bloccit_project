f7e8fee8e4406175d5c442b3bc7305a28382a24c
Kelly O'Briant <kellyobriant@gmail.com>
2015-03-06 01:21:53 -0500

    epic voting lesson

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe Vote do 
 	describe "validations" do
 		describe "value validation" do
+			before{@invalid_vote = Vote.create(value: 5)}
 			it "only allows -1 or 1 as values" do
-				expect( @vote.value ).to eq(1 || -1)
+				expect(@invalid_vote.valid?).to eq false
 			end
 		end
 	end
