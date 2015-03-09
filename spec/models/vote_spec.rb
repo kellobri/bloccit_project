34af9cd4fc0ed53a27367f7f9aa04d2fb968b49b
Kelly O'Briant <kellyobriant@gmail.com>
2015-03-08 20:26:19 -0400

    Mocking Specs checkpoint assignment

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe Vote do 
+
+	include TestFactories
+
 	describe "validations" do
 		describe "value validation" do
 			it "only allows -1 or 1 as values" do
@@ -26,25 +29,8 @@ describe Vote do
 	end
 end			
 
-def associated_post(options={})
-	post_options = {
-		title: 'Post Title',
-		body: 'Post bodies must be pretty long.',
-		topic: Topic.create(name: 'Topic name'),
-		user: authenticated_user
-	}.merge(options)
-	Post.create(post_options)
-end
 
-def authenticated_user(options={})
-	user_options = {
-		email: "email#{rand}@fake.com",
-		password: 'password'
-	}.merge(options)
-	user = User.new(user_options)
-	user.skip_confirmation!
-	user.save
-	user
-end
+
+
 
 	
