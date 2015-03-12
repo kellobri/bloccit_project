6b65c58df061d575b769a1a8b6f5b4f0cd9ff8eb
Kelly O'Briant <kellyobriant@gmail.com>
2015-03-12 17:15:59 -0400

    public profiles checkpoint lesson

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'topics/edit'
 
   devise_for :users
-  resources :users, only: [:update]
+  resources :users, only: [:update, :show]
 
   resources :topics do
     resources :posts, except: [:index]
