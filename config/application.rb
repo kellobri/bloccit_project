ba7a6ad0152de8efdd1963fa2c0f79a7379c5473
Kelly O'Briant <kellyobriant@gmail.com>
2015-03-13 00:47:34 -0400

    fixed missing host to link to error

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module Bloccit
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
   end
 end
