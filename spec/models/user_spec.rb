35d5f6fb7984da96c3c4bab763e03f60ff65b25e
Kelly O'Briant <kellyobriant@gmail.com>
2015-03-14 23:58:08 -0400

    added user_with_post_and_comment factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,16 @@ require 'rails_helper'
 describe User do
 
 	before do 
-		@user1 = create(:user)
-		@post1 = create(:post, user: @user1)
-		create(:comment, user: @user1, post: @post1)
+		@user1, @post1, @comment1 = create(:user_with_post_and_comment)
 
-		@user2 = create(:user)
-		@post2 = create(:post, user: @user2)
+		@user2, @post2, @comment2 = create(:user_with_post_and_comment)
 		2.times { create(:comment, user: @user2, post: @post2) }
 	end
 
 	describe "#favorited(post)" do
-		it "returns `nil` if the user has not favorited the post" do
-			expect( @user1.favorited(@post2) ).to be_nil
+
+		xit "returns `nil` if the user has not favorited the post" do
+			
 		end
 
 		xit "returns the appropriate favorite if it exists" do
@@ -34,7 +32,7 @@ describe User do
 
 		it "stores a `comments_count` on user" do
 			users = User.top_rated
-			expect( users.first.comments_count ).to eq(2)
+			expect( users.first.comments_count ).to eq(3)
 		end
 	end
 end
